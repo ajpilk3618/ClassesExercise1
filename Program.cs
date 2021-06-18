eaba9cee34b997caf6426fb1a20d82d4502085ff
AJ Pilkerton <apilkerton3618@gmail.com>
2021-06-18 13:17:17 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassesExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car()
+            {
+                Make = "Mercury",
+                Model = "Montego",
+                Year = 2007
+            };
+            car1.Info();
+        }
+    }
+}
